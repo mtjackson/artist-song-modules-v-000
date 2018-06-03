fc2469e53685891a0f7fc840328566f794bffc95
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-03 14:42:08 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,9 +1,6 @@
 module Findable
-  #  def find_by_name(name)
-  #    all.detect{|a| a.name == name}
-  #  end
+  def find_by_name(name)
+    all.detect{|a| a.name == name}
+  end
 
-  #  def self.find_by_name(name)
-  #    @@songs.detect{|a| a.name == name}
-  #  end
 end
