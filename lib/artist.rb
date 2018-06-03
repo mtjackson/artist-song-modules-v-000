8e546c4b5f0abf80a6afed9a6b80601f2a381429
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-03 14:54:37 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,7 +6,7 @@ require_relative '../lib/concerns/peramable.rb'
 class Artist
   extend Memorable
   extend Findable
-  include Peramable
+  extend Peramable
 
   attr_accessor :name
   attr_reader :songs
