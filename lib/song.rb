220a0f09b65703ae4d77d4014a0ac723519508bb
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-03 14:44:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 require 'pry'
 require_relative '../lib/concerns/memorable.rb'
 require_relative '../lib/concerns/findable.rb'
-
+require_relative '../lib/concerns/peramable.rb'
 
 class Song
   extend Memorable
